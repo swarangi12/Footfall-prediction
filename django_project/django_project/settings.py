03b8344ec9924122aaf4868fe1513a1eb29e2942
Swarangi Ahire <swarangi.ahire24@sakec.ac.in>
2026-08-22 23:52:44 +0530

    Fix static files configuration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
